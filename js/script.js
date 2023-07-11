3ed3ddd128ed406181f9f9f247d5fc69cad7863f
rayrcf <raymund.ricabar@celo.org>
2023-07-12 07:20:00 +0800

    initial commit

--- js/script.js
+++ js/script.js
@@ -249,3 +249,9 @@ $(document).ready(function () {
 	});
   });
 
+// Popups
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.modal');
+	var instances = M.Modal.init(elems);
+  });
+
